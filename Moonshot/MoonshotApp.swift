0c755dc0d91399a4a62e330bf54820750e186d14
ra1z <purnamaanrai7@gmail.com>
2025-09-07 18:28:43 +0545

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Purnaman Rai (College) on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
